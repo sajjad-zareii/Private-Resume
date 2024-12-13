12d66be0e697ebd08e1389d4048351b32fb8f991
Sajjad Zarei <70701962+sajjad-zareii@users.noreply.github.com>
2024-12-13 19:34:56 +0100

    Delete home directory

--- home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Message, Article, News, Category
-
-admin.site.register(Message)
-admin.site.register(Article)
-admin.site.register(News)
-admin.site.register(Category)
-
-
